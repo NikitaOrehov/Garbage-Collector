71dd181c7ce4ce12be22a138f2be64ffff0ff1d6
NikitaOrehov <Ornik0005@yandex.ru>
2024-11-19 21:21:09 +0300

    19.11.2024

--- Garbage_collector.h
+++ Garbage_collector.h
@@ -11,43 +11,75 @@ struct Object
 
 class Garbage_Collector{
 private:
-    static std::vector<Object> _AllObject;
-    static Allocator _allocator;
     void Mark();
     void Sweep();
 public:
+    static Garbage_Collector Collector;
     static int count;
+    static std::vector<Object> _AllObject;
+    static Allocator _Allocator;
+    static void AddElement(void* reference){
+        Object o;
+        o.IsAlive = 0;
+        o.Referense = reference;
+        Collector._AllObject.push_back(o);
+    }
+    
+    static void DeleteElement(void* referense){
+        for (auto i = _AllObject.begin(); i != _AllObject.end(); i++){
+            if (i->Referense == referense){
+                _AllObject.erase(i);
+                return;
+            }
+        }
+        std::cerr << "Warning: Attempt to unregister unregistered pointer: " << referense << std::endl;
+    }
+
     Garbage_Collector(){
         std::cout<<"constructor Collector\n";
     }
 
-    void GC();
+    void GC(){
 
-    friend void* operator new(size_t n){
-        std::cout<<"operator new  size = "<<n<<"\n";
-        count++;
-        Object o;
-        o.IsAlive = 0;
-        o.Referense = malloc(n);
-        // _AllObject.push_back(o);
-        // o.Referense = _allocator.GetMemory(n);
-        _AllObject.push_back(o);
-        return o.Referense;
     }
 
-    friend void operator delete(void* ptr, size_t n){
-        std::cout<<"operator delete"<<"\n";
-        free(ptr);
-    }
-
-    friend void* operator new[](size_t n);
-
-    friend void operator delete[](void* ptr);
-
-    void PrintAllObject(){
-        for (int i = 0; i < _AllObject.size(); i++){
-            std::cout<<_AllObject[i].Referense<<" ";
+    static void PrintAll(){
+        for (int i = 0; i < Collector._AllObject.size(); i++){
+            std::cout<<_AllObject[i].Referense<<"\n";
         }
-        std::cout<<"\n";
     }
 };
+
+// void* operator new(size_t size){
+//     std::cout<<"operator new "<<size<<"\n";
+//     void* referense = malloc(size);
+//     if (!referense){
+//         throw std::bad_alloc();
+//     }
+//     Garbage_Collector::AddElement(referense);
+//     return referense;
+// }
+
+// void operator delete(void* referense){
+//     std::cout<<"operator delete\n";
+//     Garbage_Collector::DeleteElement(referense);
+// }
+
+void* operator new[](size_t size){
+    std::cout<<"operator new[] :"<<size<<"\n";
+    void* referense = malloc(size);
+    if (!referense){
+        throw std::bad_alloc();
+    }
+    Garbage_Collector::AddElement(referense);
+    return referense;
+}
+
+void operator delete[](void* ptr){
+    std::cout << "operator delete[] called\n";
+    Garbage_Collector::DeleteElement(ptr);
+    free(ptr);
+}
+
+// #define new new(__FILE__)
+
--- main.cpp
+++ main.cpp
@@ -2,23 +2,18 @@
 #include <iostream>
 #include "Garbage_collector.h"
 
-Garbage_Collector a;
-Allocator Garbage_Collector::_allocator;
+Garbage_Collector Garbage_Collector::Collector;
+Allocator Garbage_Collector::_Allocator;
 std::vector<Object> Garbage_Collector::_AllObject;
 int Garbage_Collector::count = 0;
 
 int main(){
-    int* b = new int;
-    double* c = new double;
-    int* e = new int;
-    *b = 2;
-    *c = 3;
-    *e = 4;
-    std::cout<<"b = "<<*b<<"\n";
-    std::cout<<"c = "<<*c<<"\n";
-    std::cout<<"e = "<<*e<<"\n";
-    delete b;
-    delete e;
-    delete c;
-    std::cout<<a.count<<"\n";
+    int* a = new int;
+    *a = 3;
+    delete a;
+    int* b = new int[10];
+    int* c = new int[10];
+    Garbage_Collector::PrintAll();
+    delete [] b;
+    Garbage_Collector::PrintAll();
 }
